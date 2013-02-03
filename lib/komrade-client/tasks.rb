f5575ae87339a0df51be1470de6f01d19913eff5
♠ ace hacker <ryan@heroku.com>
2013-02-02 16:19:02 -0800

    fix rake task again and rev

--- lib/komrade-client/tasks.rb
+++ lib/komrade-client/tasks.rb
@@ -1,4 +1,5 @@
 require 'komrade-client/worker'
+require 'komrade-client/queue'
 
 task :environment
 
@@ -11,13 +12,8 @@ namespace :komrade do
     @worker.start
   end
 
-  desc "Returns the number of jobs in the queue."
-  task :count => :environment do
-    $stdout.puts(Komrade::Worker.new.queue.count)
-  end
-
   desc "Deletes all jobs in the queue."
   task :delete_all => :environment do
-    $stdout.puts(Komrade::Worker.new.queue.delete_all)
+    $stdout.puts(Komrade::Queue.delete_all)
   end
 end
